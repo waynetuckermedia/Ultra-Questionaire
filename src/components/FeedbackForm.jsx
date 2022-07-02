2ca74ee030081979fc8e3692b292fb1e29b476b0
Wayne <waynetuckermedia@gmail.com>
2022-07-02 04:41:55 -0400

    added character limit for button

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,22 @@ import Button from "./shared/Button"
 
 function FeedbackForm() {
     const[text, setText] = useState('')
+    const[btnDisabled, setBtnDisabled] = useState('true')
+    const[message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+        if(text === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if(text !== '' && text.trim().length <= 10){
+        setMessage('Text must be 10 characters or less')
+        setBtnDisabled(true)
+        }else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+
+
         setText(e.target.value)
     }
 
@@ -17,8 +31,9 @@ function FeedbackForm() {
        {/* @todo - rating select component */}
        <div className="input-group">
         <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
-        <Button type="submit">Send</Button>
+        <Button type="submit" isDisabled={btnDisabled}>Send</Button>
        </div>
+       {message && <div className='message'>{message}</div>}
     </Card>
   )
 }
